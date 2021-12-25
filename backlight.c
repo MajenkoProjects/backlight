7afb3417270171a7c212fa31658500926b161302
Matt Jenkins <matt@majenko.co.uk>
2021-12-25 16:10:37 +0000

    Added delta +- adjustments

--- backlight.c
+++ backlight.c
@@ -3,28 +3,48 @@
 
 const char *root = "/sys/class/backlight/intel_backlight";
 
-int main(int argc, char **argv) {
+int getValue(const char *item) {
 	char temp[1024];
-
-	snprintf(temp, 1024, "%s/max_brightness", root);
+	snprintf(temp, 1024, "%s/%s", root, item);
 	FILE *f = fopen(temp, "r");
+	if (!f) {
+		return -1;
+	}
 	fgets(temp, 1024, f);
 	fclose(f);
-	int maxb = strtol(temp, NULL, 10);
+	return strtol(temp, NULL, 10);
+}
+
+int main(int argc, char **argv) {
+	char temp[1024];
 
 	if (argc != 2) {
 		printf("Usage: backlight <percent>\n");
 		return -1;
 	}
 
-	int pct = strtol(argv[1], NULL, 10);
-	if (pct < 0) pct = 0;
-	if (pct > 100) pct = 100;
+	int max_brightness = getValue("max_brightness");
+	int current_brightness = getValue("brightness");
 
-	int out = maxb * pct / 100;
+	int current_percent = current_brightness * 100 / max_brightness;
+
+	int percent = current_percent;
+
+	if (argv[1][0] == '+') {
+		percent += strtol(argv[1]+1, NULL, 10);
+	} else if (argv[1][0] == '-') {
+		percent -= strtol(argv[1]+1, NULL, 10);
+	} else {
+		percent = strtol(argv[1], NULL, 10);
+	}
+
+	if (percent < 0) percent = 0;
+	if (percent > 100) percent = 100;
+
+	int out = max_brightness * percent / 100;
 
 	snprintf(temp, 1024, "%s/brightness", root);
-	f = fopen(temp, "w");
+	FILE *f = fopen(temp, "w");
 	if (!f) {
 		printf("Unable to open %s\n", temp);
 		return -1;
